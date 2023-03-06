be5584d15791083e9043280ae17929a35e297734
Philippe Dumonet <philogy@Philippes-MacBook-Air.local>
2023-03-05 17:16:04 -0700

    Make generated constraints nicer

--- gen/block_constraints.py
+++ gen/block_constraints.py
@@ -6,11 +6,16 @@ def main():
     for x in range(3):
         for y in range(3):
             base_ind = 27 * x + 3 * y
+            cs.append((x, y, []))
             for a in range(9):
                 for b in range(a + 1, 9):
-                    cs.append((base_ind + offsets[a], base_ind + offsets[b]))
-    for i, j in cs:
-        print(f'constrain solution[{i}] == solution[{j}];')
+                    cs[-1][2].append(
+                        (base_ind + offsets[a], base_ind + offsets[b])
+                    )
+    for x, y, block_constraints in cs:
+        print(f'\n  // block({x + 1}, {y + 1})')
+        for i, j in block_constraints:
+            print(f'  constrain solution[{i}] == solution[{j}];')
 
 
 if __name__ == '__main__':
